16b298109a118d0f21af4d24a129386e97d359e5
Maksym Kikot <maxym1111111@gmail.com>
2023-06-21 04:23:24 +0300

    add: full hw

---
+++ README.md
@@ -0,0 +1,11 @@
+Получаем и выводим весь список контактов в виде таблицы (console.table)
+https://monosnap.com/file/BeOY5szD8qIQWN1FPmjbcKcBbFfTqM
+
+Получаем контакт по id - выводим в консоль объект контакта или null, если контакта с таким id не существует.
+https://monosnap.com/file/LQgQDYZV7hVU4QfkSDdoYlPLsoowXh
+
+Добавляем контакт и выводим в консоль созданный контакт
+https://monosnap.com/file/5lurl4PmmPMpHpHO0mBtGtSs3L9Qje
+
+Удаляем контакт и выводим в консоль удаленный контакт или null, если контакта с таким id не существует.
+https://monosnap.com/file/DHxQRO8KRErZl8KhamRzWXau0qelhz
---
+++ contacts.js
@@ -0,0 +1,95 @@
+const { readFile, writeFile } = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join("db", "contacts.json");
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  // Возвращает массив контактов.
+  try {
+    const contacts = await readFile(contactsPath);
+    const jsonContacts = JSON.parse(contacts);
+
+    console.table(jsonContacts);
+    return jsonContacts;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getContactById(contactId) {
+  // Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
+  const allContacts = await readFile(contactsPath);
+
+  const jsonContacts = JSON.parse(allContacts);
+
+  const curContact = jsonContacts.find((contact) => contact.id === contactId);
+
+  if (curContact === undefined) {
+    console.log(null);
+    return null;
+  }
+
+  console.log(curContact);
+  return curContact;
+}
+
+async function removeContact(contactId) {
+  // Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
+  const newPathFile = path.join("db", "newContacts.json");
+  let removedContact = null;
+
+  const allContacts = await readFile(contactsPath);
+  const jsonContacts = JSON.parse(allContacts);
+
+  const newContacts = jsonContacts.filter((contact) => {
+    if (contact.id === contactId) {
+      removedContact = contact;
+    }
+    return contact.id !== contactId;
+  });
+
+  if (newContacts.length === jsonContacts.length) {
+    console.log(null);
+    return null;
+  }
+
+  await writeFile(newPathFile, JSON.stringify(newContacts));
+
+  const removedContacts = await readFile(newPathFile);
+
+  // console.log(JSON.parse(removedContacts));
+  console.log(removedContact);
+
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  // Возвращает объект добавленного контакта.
+  const newPathFile = path.join("db", "newContacts.json");
+  const createdContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  try {
+    const allContacts = await readFile(contactsPath);
+    const parsedContacts = JSON.parse(allContacts);
+
+    parsedContacts.push(createdContact);
+    await writeFile(newPathFile, JSON.stringify(parsedContacts));
+
+    const newContacts = await readFile(newPathFile);
+    const newParsedContacts = JSON.parse(newContacts);
+
+    console.log(createdContact);
+    return createdContact;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
